ca85d806d79defe1dcd16c84e66298669a5150e2
WhichWho <1366505103@qq.com>
2023-09-12 11:44:00 +0800

    fix gumroad null key.

--- src/main/kotlin/loli/ball/kemono/bean/KemonoPost.kt
+++ src/main/kotlin/loli/ball/kemono/bean/KemonoPost.kt
@@ -17,13 +17,13 @@ data class KemonoPost(
     val added: String,                  //收藏时间
     val attachments: List<Attachment>,  //收藏的附件 可能为空
     val content: String,                //作品简介html
-    val edited: String = "",            //编辑时间
+    val edited: String? = null,         //编辑时间
 //    val embed: Any? = null,           //ignore
     val faved_seq: Int = 0,             //收藏的顺序
     @Serializable(with = AttachmentSerializer::class)
     val file: Attachment?,              //收藏的附件 封面预览图
     val id: String,                     //作品的id
-    val published: String,              //发布时间
+    val published: String? = null,      //发布时间
     val service: String,                //隶属于的服务器 详见ArtistService
     val shared_file: Boolean,           //未知 通常是false
     val title: String,                  //作品的标题
